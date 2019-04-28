3ffcf787d9a2844bd5cf324de20fc5baa24ac912
Ahad2906 <s175128@student.dtu.dk>
2019-04-28 21:12:22 +0200

    hurtigt

--- app/src/main/java/dk/gruppe5/koerskolepriser/fragmenter/OpretTilbudFragment.java
+++ app/src/main/java/dk/gruppe5/koerskolepriser/fragmenter/OpretTilbudFragment.java
@@ -1,16 +1,26 @@
 package dk.gruppe5.koerskolepriser.fragmenter;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import dk.gruppe5.koerskolepriser.R;
+import dk.gruppe5.koerskolepriser.objekter.Soegning;
 
 public class OpretTilbudFragment extends Fragment {
+
+    private TextView txt_filtre;
+    private EditText etxt_post;
+    private View layout_extra;
+    private CheckBox cbox_mand, cbox_kvinde, cbox_lyn;
+    private Soegning søgning;
+    private Spinner sp_pris, sp_type, sp_mærke, sp_størrelse, sp_dag;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
