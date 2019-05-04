0f797b42414aeffcb4210c520b7b9e761c5e4200
Nils Rasamoel <nilsrasa>
2019-05-04 14:38:28 +0200

    RXjava

--- app/src/main/java/dk/gruppe5/koerskolepriser/APIKlient.java
+++ app/src/main/java/dk/gruppe5/koerskolepriser/APIKlient.java
@@ -1,7 +1,5 @@
 package dk.gruppe5.koerskolepriser;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -12,18 +10,12 @@ public class APIKlient {
 
     static Retrofit getKlient() {
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-
         retrofit = new Retrofit.Builder()
                 .baseUrl("https://reqres.in")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(client)
                 .build();
 
         return retrofit;
     }
-
 }
