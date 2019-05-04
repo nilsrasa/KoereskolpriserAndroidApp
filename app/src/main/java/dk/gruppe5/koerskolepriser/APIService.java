087f2a4a93184b57006baa2119bce30b04c766fb
Ahad2906 <s175128@student.dtu.dk>
2019-05-04 15:26:32 +0200

    Retrofit APIService - hentAlleTilbud og Postnummer

--- app/src/main/java/dk/gruppe5/koerskolepriser/APIService.java
+++ app/src/main/java/dk/gruppe5/koerskolepriser/APIService.java
@@ -3,13 +3,16 @@ package dk.gruppe5.koerskolepriser;
 import dk.gruppe5.koerskolepriser.objekter.Tilbud;
 import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface APIService {
 
-    @GET("tilbudTilBruger/{koreskole_id}")
-    Single<Tilbud> getTilbudData(@Path("koreskole_id") int tilbudId,
-                                 @Query("api_key") String apiKey);
+    @GET("webresources/generic/getAlleTilbud")
+    Single<Tilbud> getAlleTilbudData();
+
+    @POST("webresources/generic/tilbudMedGiventPostnummer/{postnummer}")
+    Single<Tilbud> hentTilbudMedPostnummer(@Path("postnummer") int postnummer);
 
 }
