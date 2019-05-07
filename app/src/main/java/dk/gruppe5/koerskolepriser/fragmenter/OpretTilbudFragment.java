66c90027690b6ce3f5cc7cff79c86578f0d4d532
Nils Rasamoel <nils.rasamoel@gmail.com>
2019-05-07 15:13:09 +0200

    sout

--- app/src/main/java/dk/gruppe5/koerskolepriser/fragmenter/OpretTilbudFragment.java
+++ app/src/main/java/dk/gruppe5/koerskolepriser/fragmenter/OpretTilbudFragment.java
@@ -125,6 +125,7 @@ public class OpretTilbudFragment extends Fragment implements View.OnClickListene
         for (int i=0; i<arr.length; i++) {
             System.out.print(arr[i]);
         }
+        System.out.println();
 
     }
 
